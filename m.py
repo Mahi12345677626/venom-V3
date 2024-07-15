99c10061c7ee8611d43affedb7b2907e0716ea83
Mahi12345677626 <mahisharma12211@gmail.com>
2024-07-15 13:27:18 +0530

    Update m.py

--- m.py
+++ m.py
@@ -10,9 +10,9 @@ from keep_alive import keep_alive
 keep_alive()
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEM')
+bot = telebot.TeleBot('7406099385:AAH8Q21RQUpSCpnoI5bkX5FkfAQaBCEvq_Y')
 # Admin user IDs
-admin_id = {"5588464519"}
+admin_id = {"6918023381"}
 
 # Files for data storage
 USER_FILE = "users.json"
